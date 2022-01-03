b7a1c6e2f5022f26dc3d0869b1395d1100c236f6
onee <onee.me@outlook.com>
2022-01-03 21:42:21 +0800

    feat: implement A

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 (function main() {
     const [canvas, context] = setupCanvas();
+    drawA(canvas, context);
 })();
 
 function setupCanvas() {
@@ -13,3 +14,17 @@ function setupCanvas() {
     context.fillRect(0, 0, canvas.width, canvas.height);
     return [canvas, context];
 }
+
+function drawA(canvas, context) {
+    context.fillStyle = 'green';
+    const rectWidth = canvas.width / 3;
+    const rectHeight = canvas.height;
+    context.fillRect(rectWidth, 0, rectWidth, rectHeight);
+
+    const textSize = 20;
+    context.font = `${textSize}px`;
+    context.fillStyle = 'black';
+    context.textAlign = 'center';
+    context.fillText('A', canvas.width / 2, canvas.height / 2);
+}
+
